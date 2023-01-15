6942d90a7585506775ce02ef968786c5074d3f61
Romit Sutariya <romitsutariya@gmail.com>
2023-01-15 16:41:29 +0530

    home page added
    Application was throwing error like page not found as it does not have any home page , added home page

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
+const path = require('path');
   
 require("dotenv").config();
 app.use(express.json({ limit: "50mb", extended: false}));
@@ -10,8 +11,19 @@ connection.once('open', () => console.log('DB Connected'))
 connection.on('error', () => console.log('Error'))
 
  //parse incoming request body in JSON format.
-app.use('/', require('./routes/redirect'))
+app.use('/s', require('./routes/redirect'))
 app.use('/api/url', require('./routes/url'))
+
+//serve static page
+app.get('/', (req, res) => {
+    try {
+        res.sendFile(path.join(__dirname, '/index.html'));
+    }
+    catch (err) {
+        console.error(err)
+        res.status(500).json('Server Error | Checl log for more details')
+    }
+})
 
 //Listen for incoming requests
 const PORT = process.env.PORT || 5000
